7c3e0ca956bd06e7b2b53160e076b8d6dda41fd4
Yvis Freire <freireyvis@gmail.com>
2022-01-30 14:47:00 -0300

    fixed menu/credits

--- levels/Credits.gd
+++ levels/Credits.gd
@@ -8,7 +8,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$VBoxContainer/Start.grab_focus()
+	$VBoxContainer/Quit.grab_focus()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,11 +16,5 @@ func _ready():
 #	pass
 
 
-func _on_Start_pressed():
-	get_tree().change_scene("res://levels/Level_0.tscn")
-
-func _on_Credits_pressed():
-	get_tree().change_scene("res://levels/Credits.tscn")
-
 func _on_Quit_pressed():
-	get_tree().quit()
+	get_tree().change_scene("res://levels/Menu.tscn")
